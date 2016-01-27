d33e4ac986b90ea4e76223f9d91d7020e4822f44
Yoichiro Shimizu <yoichi.simizu@gmail.com>
2016-01-27 17:44:39 +0900

    Form:to do send.php

---
+++ mail/check.php
@@ -0,0 +1,48 @@
+﻿<?php
+//不正アクセスチェック。
+//!$_POSTで　$_POSTがSetされているかをチェックし、セットされていなければリダイレクト
+if(!$_POST || !isset($_POST["name"]) || !isset($_POST["e_mail"]) || !isset($_POST["comment"])){
+	header('Location:http://localhost/develop/mail/mail/mail.php');//リダイレクト処理、テスト用アドレス
+		}
+
+session_start();
+$_SESSION = $_POST;//to do：正規表現によるチェック等
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>CONFIRM</title>
+</head>
+
+<body>
+	<form id="form-check" action="./send.php" method="post">
+					<input type="hidden" name="sub_actions" value="confirm">
+					<table>
+					<tbody>
+					<tr>
+					<th>Name</th>
+					<td><?php echo htmlspecialchars($_POST['name']);?></td>
+					</tr>
+					<tr>
+					<th>Mail</th>
+					<td><?php echo htmlspecialchars($_POST['e_mail']);?></td>
+					</tr>
+					<tr>
+					<th style="vertical-align:top;">Comment</th>
+					<td><?php echo htmlspecialchars($_POST['comment']);?>
+					</td>
+					</tr>
+					</tbody>
+					</table>
+
+	<div class="bt">
+	<input type="button" value="戻る" onclick="history.back()">
+	<input type="submit" value="送信" name="submit">
+	</div>
+</form>
+</body>
+</html>
---
+++ mail/mail.php
@@ -0,0 +1,43 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>CONTACT</title>
+<link rel="stylesheet" href="./css/validationEngine.jquery.css" type="text/css"/>
+<script type="text/javascript" src="./valid/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="./valid/jquery.validationEngine.js"></script>
+<script type="text/javascript" src="./valid/jquery.validationEngine-ja.js"></script>
+<script type="text/javascript">
+$(function(){
+   jQuery("#contact-form").validationEngine();
+});
+</script>
+
+</head>
+
+<body>
+  <form id="contact-form" action="./check.php" method="post">
+					<input type="hidden" name="sub_actions" value="confirm">
+					<table>
+					<tbody>
+					<tr>
+					<th>Name</th>
+    					<td><input type="text" class="validate[required]" name="name" value=""required></td>
+					</tr>
+					<tr>
+					<th>Mail</th>
+					<td><input type="text" class="validate[required,custom[email]]" name="e_mail" value=""required></td>
+					</tr>
+					<tr>
+					<th style="vertical-align:top;">Comment</th>
+					<td><textarea name="comment" rows="30" cols="40" maxlength="1200" class="[required,maxSize[1200]]"required></textarea>
+					</td>
+					</tr>
+					</tbody>
+					</table>
+  <div class="bt">
+  <input type="submit" value="Confirm" name="submit">
+  </div>
+  </form>
+</body>
+</html>
